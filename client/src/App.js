4cd42a153bfaef1c6c124fc15e53ff1bea2f1f3a
vongola97 <tunahanoezsoy@gmail.com>
2024-04-15 23:58:53 +0200

    implemented toggling of completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,52 @@
+import { useEffect, useState } from 'react';
+const api_base = 'http://localhost:3001';
+
 function App() {
+  const [todos, setTodos] = useState([]);
+	const [popupActive, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState("");
+
+	useEffect(() => {
+		GetTodos();
+	}, []);
+
+	const GetTodos = () => {
+		fetch(api_base + '/todos')
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch((err) => console.error("Error: ", err));
+	}
+
+  const completeTodo = async id => {
+		const data = await fetch(api_base + '/todo/complete/' + id).then(res => res.json());
+
+		setTodos(todos => todos.map(todo => {
+			if (todo._id === data._id) {
+				todo.complete = data.complete;
+			}
+
+			return todo;
+		}));
+		
+	}
+
   return (
     <div className="App">
       <h1>Welcome</h1>
       <h4>Your Tasks</h4>
       <div className="todos">
-
-        <div className="todo">
+        {todos.map(todo => (
+          <div className={
+            "todo " + (todo.complete ? "is-complete" : "")
+            } key={todo._id} onClick={() => completeTodo(todo._id)}>
           <div className="checkbox"></div>
 
-          <div className="text">Get the bread</div>
+          <div className="text">{ todo.text }</div>
           <div className="delete-todo">x</div>
         </div>
-
-        <div className="todo is-complete">
-          <div className="checkbox"></div>
-
-          <div className="text">Get the milk</div>
-          <div className="delete-todo">x</div>
-        </div>
+        ))}
         
+
       </div>
     </div>
   );
