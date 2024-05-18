20ecfdffc30b8f98b2752c9ebe4079d575fbf86f
vongola97 <tunahanoezsoy@gmail.com>
2024-05-18 20:16:15 +0200

    Implemented expandable cards

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
 	const [todos, setTodos] = useState([]);
 	const [popupActive, setPopupActive] = useState(false);
 	const [newTodo, setNewTodo] = useState("");
+	const [viewTodo, setViewTodo] = useState(null);  // State for tracking the viewed todo
 	const gridRef = useRef(null);  // Reference to the grid container
 
 	useEffect(() => {
@@ -67,17 +68,30 @@ function App() {
 		setNewTodo("");
 	}
 
+	const openViewTodo = (todo) => {
+        setViewTodo(todo);
+        setPopupActive(true);
+    }
+
+    const closeViewTodo = () => {
+        setViewTodo(null);
+        setPopupActive(false);
+    }
+
+
 
 	return (
 		<div className="App">
-			{popupActive && <div className="overlay" onClick={() => setPopupActive(false)}></div>}
+			{popupActive && <div className="overlay" onClick={closeViewTodo}></div>}
 			<h1>Welcome</h1>
 			<h4>Your Tasks</h4>
 			<div ref={gridRef} className="todos">
 				{todos.map(todo => (
 					<div className={
 						"todo " + (todo.complete ? "is-complete" : "")
-					} key={todo._id}>
+					} key={todo._id}
+					onClick={() => openViewTodo(todo)} // Open the popup on click
+					>
 						<input
 							type="checkbox"
 							checked={todo.complete}
@@ -95,20 +109,30 @@ function App() {
 
 			<div className='addPopup' onClick={() => setPopupActive(true)}>+</div>
 
-			{popupActive ? (
-				<div className="popup">
-					<div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
-					<div className="content">
-						<h3>Add Task</h3>
-						<input
-							type="text"
-							className="add-todo-input"
-							onChange={e => setNewTodo(e.target.value)}
-							value={newTodo} />
-						<div className="button" onClick={addTodo}>Create Task</div>
-					</div>
-				</div>
-			) : ''}
+			{popupActive && viewTodo && (
+                <div className="popup">
+                    <div className="closePopup" onClick={closeViewTodo}>X</div>
+                    <div className="content">
+                        <h3>Task Details</h3>
+                        <div className="todo-text">{viewTodo.text}</div>
+                    </div>
+                </div>
+            )}
+
+            {popupActive && !viewTodo && (
+                <div className="popup">
+                    <div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
+                    <div className="content">
+                        <h3>Add Task</h3>
+                        <input
+                            type="text"
+                            className="add-todo-input"
+                            onChange={e => setNewTodo(e.target.value)}
+                            value={newTodo} />
+                        <div className="button" onClick={addTodo}>Create Task</div>
+                    </div>
+                </div>
+            )}
 		</div>
 	);
 }
